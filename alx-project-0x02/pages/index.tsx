30bbc50633c124a1937f27fa8df7243b351005bd
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 13:56:43 +0100

    Create index.tsx

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <h1>Welcome to ALX Project 0x02 Setup</h1>
+      <p>This is a sample homepage created using GitHub UI only.</p>
+    </Layout>
+  );
+}
