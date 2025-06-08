25a9abe5d1cafd7805eb391d9b091b507f6d2c4c
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 15:57:14 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
+
 export default function Header() {
   return (
-    <nav>
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/posts">Posts</Link>
-      <Link href="/users">Users</Link>
+    <nav className="p-4 bg-gray-100 mb-6">
+      <ul className="flex space-x-4">
+        <li><Link href="/home">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/posts">Posts</Link></li>
+        <li><Link href="/users">Users</Link></li>
+      </ul>
     </nav>
-  )
+  );
 }
