31f46d7ea2dabf6ee8bedb1184e06a5de571c1dc
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 16:04:24 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Header />
 
       <h1 className="text-2xl font-bold mb-4">Home Page</h1>
-      {/* rest of your HomePage content */}
+      {}
     </div>
   );
 }
