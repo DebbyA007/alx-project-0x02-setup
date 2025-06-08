0bc8ee1f37908c8654dad4a16c2e347902e18446
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 16:12:58 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -3,11 +3,15 @@ import Button from '../components/common/Button';
 
 export default function AboutPage() {
   return (
-    <div>
+    <div className="p-6">
       <Header />
 
       <h1 className="text-2xl font-bold mb-4">About Page</h1>
-      {}
+      <div className="space-x-4">
+        <Button size="small" shape="rounded-sm">Small / rounded-sm</Button>
+        <Button size="medium" shape="rounded-md">Medium / rounded-md</Button>
+        <Button size="large" shape="rounded-full">Large / rounded-full</Button>
+      </div>
     </div>
   );
 }
