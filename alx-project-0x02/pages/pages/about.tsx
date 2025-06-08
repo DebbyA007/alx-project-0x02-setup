3d0fdf7067ae9580acfc749c5a68cf6194dabdbd
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 15:46:13 +0100

    Update about.tsx

--- alx-project-0x02/pages/pages/about.tsx
+++ alx-project-0x02/pages/pages/about.tsx
@@ -1,10 +1,26 @@
-import Layout from "../components/layout/Layout";
+import React from 'react';
+import Header from '../components/layout/Header';
+import Button from '../components/common/Button';
 
-export default function About() {
+export default function AboutPage() {
   return (
-    <Layout>
-      <h1>About</h1>
-      <p>This is the About page. It tells about this project.</p>
-    </Layout>
+    <div className="p-6">
+      <Header />
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+
+      <div className="space-x-4">
+        <Button size="small" shape="rounded-sm">
+          Small / rounded-sm
+        </Button>
+
+        <Button size="medium" shape="rounded-md">
+          Medium / rounded-md
+        </Button>
+
+        <Button size="large" shape="rounded-full">
+          Large / rounded-full
+        </Button>
+      </div>
+    </div>
   );
 }
