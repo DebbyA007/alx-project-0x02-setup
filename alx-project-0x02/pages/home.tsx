43f66199308af815f608269c322a16a940a6dbdd
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 14:00:16 +0100

    Create home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+      <p>This is the Home page content.</p>
+    </Layout>
+  );
+}
