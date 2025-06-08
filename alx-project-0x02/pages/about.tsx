a0ed5d51de5a598388ca52d4688b669ca1381223
Deborah Akinbola <akinboladeborah400@gmail.com>
2025-06-08 14:25:54 +0100

    Create about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p>This is the About page. It tells about this project.</p>
+    </Layout>
+  );
+}
